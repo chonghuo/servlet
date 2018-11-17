8a4b2b82805e7e9ab166eacd0a9390f00a245a06
chonghuo <663420930@qq.com>
2018-11-17 11:07:11 +0800

    简单登陆

---
+++ SimpleLogin/src/main/java/com/tcredit/servlet/LoginServlet.java
@@ -0,0 +1,44 @@
+package com.tcredit.servlet;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ * Auther: chong.huo
+ * Create  Date: 2018/11/16
+ */
+public class LoginServlet extends GenericServlet {
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+
+        /**
+         * 登陆账号密码 root：123456
+         */
+
+
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+
+        if ("root".equals(username) && "123456".equals(password)){
+
+            HttpServletRequest request = (HttpServletRequest)req;
+
+            HttpSession session = request.getSession();
+
+            session.setAttribute("用户","mike");
+            req.getRequestDispatcher("/project").forward(req,res);
+        }else {
+            res.setCharacterEncoding("GBK");
+            res.getWriter().write("您的身份不合法");
+
+        }
+
+
+    }
+}
